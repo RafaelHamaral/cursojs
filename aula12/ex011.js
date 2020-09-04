06c9b93a1179167154b1a2df1d74b2edd45d532e
Rafael H Amaral <rafael_holandamaral@hotmail.com>
2020-09-04 20:46:17 -0300

    revisao e inicio dos exercicios

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,11 +1,16 @@
-var idade = 17
+var idade = 66
 if(idade < 16){
+
     console.log(`Nao vota`)
-} else {
-    if( idade < 18) {
+
+} else if( idade < 18 || idade > 65) {
+
         console.log('voto opcional')
-    }
+    }else {
+
     console.log(`Vota`)
+    
 }
+
 
 
