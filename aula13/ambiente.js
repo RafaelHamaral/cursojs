172eae5a60feb6c111da95e7a6499bc2eee3fc93
Rafael H Amaral <rafael_holandamaral@hotmail.com>
2020-09-23 22:15:29 -0300

    repeticoes
    while, do while e for

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,20 @@
-var c = 1
+
+//while - estrutura de repetição com teste logico no inicio
+
+// do while - estrutura de repetição com teste logico no final
+
+
+var c = 2
+
+while (c <= 6){
+   console.log(`passo ${c}`)
+   c++
+   
+} 
+
+
+var c = 1      
 do {
-    console.log(`passo ${c}`)
+    console.log(`passo ${c}`)  //crases habilita que eu possa fazer interpolação por placeholde ${c}
     c++
-} while (c <= 6) 
+} while (c <= 10) 
---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+
+var f = 1
+do {
+    console.log(` cont ${f}`)
+    f++
+
+} while (f<=6)
+
+
+
+for(f=1; f <= 6 ; f++ ){
+    console.log(f)
+}
+
