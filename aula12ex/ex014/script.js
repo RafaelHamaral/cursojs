4891b4954421ef271bfbae60dbeab2bc083f079b
Rafael H Amaral <rafael_holandamaral@hotmail.com>
2020-10-10 18:55:45 -0300

    update ex 14

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -8,12 +8,12 @@ var minuto = data.getMinutes()
 msg.innerHTML = `Agora são ${hora} horas e `+`${minuto} minutos`
     if(hora >= 0 && hora < 12) {
         //BOM DIA!
-        img.src = 'fotomanha.png'
-        document.body.style.background = '#d5d1b6'
+        img.src = 'fotomanha.png'//busca a imagem da minha pasta
+        document.body.style.background = '#d5d1b6'//corde fundo
      
     } else if (hora >= 12 && hora < 18){
         //BOA TARDE!
-        img.src = 'fototarde.png'
+        img.src = 'fototarde.png' 
         document.body.style.background = '#e49078'
     }else{
         //BOA NOITE!
