ef219e4ef1c8bdcc60cf38ec117e5a9a787df620
Rafael H Amaral <rafael_holandamaral@hotmail.com>
2020-10-12 13:32:11 -0300

    Módulo F - Variaveis compostas

---
+++ aula15/ambiente.js
@@ -0,0 +1,23 @@
+let num = [5, 8, 2, 9, 3]
+
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+
+for(let pos = 0; pos < num.length; pos ++){
+    console.log(`posição ${num[pos]}`)
+}
+
+//buscar valores dentro de um vetor (indice)  --- index.Of() *se nao encontrar ele retorna -1.
+let pos = num.indexOf(8)
+if(pos == -1){
+    console.log(`O valor não foi encontrado!`)
+}else{
+console.log(`O valor 8 esta na posição ${pos}`)
+}
+
+
+
---
+++ aula15/vetornatela.js
@@ -0,0 +1,16 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+
+valores.sort()//ordenando o codigo
+
+console.log('padrão: ')
+for(let valor = 0; valor < valores.length; valor ++){
+    console.log(`A posição ${valor} tem o valor ${valores[valor]}`)
+}
+
+//simplificando o codigo com o IN 
+console.log('')
+console.log(`Utilizando o IN: `)
+for(let valor in valores){
+    console.log(`A posição ${valor} tem o valor ${valores[valor]}`)   
+}
